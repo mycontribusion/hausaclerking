be25b5d55bbea6f5b0dfc554122382b3d0003a00
Ahmad Musa <mycontribusion01@gmail.com>
2025-08-08 10:39:58 +0100

    centralize logo on landscape mode

--- src/HausaClerking.js
+++ src/HausaClerking.js
@@ -5,7 +5,7 @@ import logo from './hclogo.jpg';
 function HausaClerking() {
     return(
         <div>
-            <div><img src={logo} alt="" width='70px' height='70px'/></div>
+            <div className="img"><img src={logo} alt="" width='70px' height='70px'/></div>
             <div>
                 <Buttons/>
                 </div>
